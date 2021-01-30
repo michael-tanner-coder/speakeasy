8bbde37d2caa229b239fabfcfa200e76fc1df1f8
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-01-30 13:28:58 -0600

    Signout now clears atom state when logging out

--- src/components/SignOut/index.tsx
+++ src/components/SignOut/index.tsx
@@ -2,16 +2,25 @@
 
 import React from "react";
 import { auth } from "../../firebase/config";
+import { useRecoilState } from "recoil";
+import { userState, chatRooms } from "../../atoms";
 import Button from "../Button";
 
 const SignOut = () => {
+  const [user, setUser] = useRecoilState(userState);
+  const [chats, setChats] = useRecoilState(chatRooms);
+
   return (
     auth.currentUser && (
       <Button
         variant="primary"
         shade="light"
         className="sign-out"
-        onClick={() => auth.signOut()}
+        onClick={() => {
+          auth.signOut();
+          setUser({});
+          setChats([]);
+        }}
       >
         Sign Out
       </Button>
