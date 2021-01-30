29d4b8068e0350324d80446799bfac794e425883
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-01-30 17:32:49 -0600

    Refactored functionality to a separate component

--- src/components/RoomCreator/index.tsx
+++ src/components/RoomCreator/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 // State + Data
 import { auth } from "../../firebase/config";
@@ -6,28 +6,18 @@ import { firestore } from "../../firebase/config";
 
 // Components
 import Button from "../Button";
-import { Wrapper, RoomLink, Input } from "./styles/RoomCreator";
+import RoomLinkSender from "../RoomLinkSender";
+import { Link } from "react-router-dom";
 
 // Types
 import Room from "../../models/Room";
 
 // Utils
 import addChatToUserProfile from "../../util/addChatToUserProfile";
-import { init, send } from "emailjs-com";
-
-// Initial process to send over emailJS
-init(process.env.REACT_APP_EMAILJS_USER_ID as string);
 
 // Main Component
 const RoomCreator = () => {
-  // State + refs
   const [link, setLink] = useState("");
-  const [invited, setInvited] = useState(false);
-  const [email, setEmail] = useState("");
-
-  const textRef = useRef<HTMLTextAreaElement>(
-    document.createElement("textarea")
-  );
 
   // Functions
   const updateUser = () => {
@@ -51,52 +41,22 @@ const RoomCreator = () => {
     }
   };
 
-  const copyText = () => {
-    console.log("copying");
-    textRef.current.select();
-    document.execCommand("copy");
-    console.log("copied");
-  };
-
-  const sendEmail = () => {
-    if (auth.currentUser) {
-      // send the message and get a callback with an error or details of the message that was sent
-      send("default_service", "template_ocqsdjy", {
-        from_name: auth.currentUser.email,
-        to_name: email,
-        invite_link: link,
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            setInvited(true);
-          }
-        })
-        // Handle errors here however you like
-        .catch((err) => console.error("Failed to send feedback. Error: ", err));
-    }
-  };
-
   // Side effects
   useEffect(() => {
     updateUser();
   }, [link]);
 
+  // Render
   return (
     <div>
       <Button onClick={() => createRoom()}>Create Room</Button>
       {link && (
-        <Wrapper>
-          <p>Room Link:</p>
-          <RoomLink ref={textRef} value={link} />
-          <Input
-            type="email"
-            onChange={({ target }) => setEmail(target.value)}
-          />
-          <Button disabled={email ? false : true} onClick={() => sendEmail()}>
-            Send
+        <>
+          <RoomLinkSender link={link} />
+          <Button>
+            <Link to={"/chat?room=" + link}>Join Chat</Link>
           </Button>
-          <Button onClick={() => copyText()}>Copy</Button>
-        </Wrapper>
+        </>
       )}
     </div>
   );
