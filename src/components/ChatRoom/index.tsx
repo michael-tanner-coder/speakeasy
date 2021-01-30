817e371f24e52b111bd6507a3cb9dfe50773683e
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-01-30 13:55:12 -0600

    Moved styled components to a separate folder

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -19,159 +19,12 @@ import Message from "../../models/Message";
 import User from "../../models/User";
 
 // Styles + Icons
-import styled from "styled-components";
+import { Tone, ToneButton, SendButton, ChatForm } from "./styles/ChatRoom";
 import ToneIcon from "../../images/ToneButton.svg";
 import SendIcon from "../../images/SendIcon.svg";
 
 // Utils
 import addChatToUserProfile from "../../util/addChatToUserProfile";
-
-// Styled components
-const Tone = styled.div`
-  p {
-    /* Default */
-    cursor: pointer;
-    padding: 0.5rem;
-    background: none;
-    border-radius: ${({ theme }) => theme.shape.border_radius};
-
-    /* States */
-    &.selected {
-      background: ${({ theme }) => theme.color.primary.light};
-      &:hover {
-        background: ${({ theme }) => theme.color.primary.light};
-      }
-    }
-
-    &:hover {
-      background: ${({ theme }) => theme.color.primary.light};
-    }
-  }
-`;
-
-const ChatForm = styled.form`
-  /* Colors */
-  background-color: ${({ theme }) => theme.color.primary.main};
-
-  /* Effects */
-  box-shadow: 0px -4px 12px rgba(0, 0, 0, 0.25);
-
-  /* Layer */
-  z-index: ${({ theme }) => theme.z_index.chatbar};
-
-  /* Size + Shape */
-  padding: 0.2rem 1rem;
-  box-sizing: border-box;
-  height: 15vh;
-  width: 100%;
-  max-width: 728px;
-
-  /* Display */
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  /* Position
-  position: fixed;
-  bottom: 0; */
-
-  /* Typography */
-  font-size: 1.5rem;
-`;
-
-const ToneButton = styled.div`
-  /* Color */
-  background-color: ${({ theme }) => theme.color.primary.light};
-
-  /* Animation */
-  transition: all 0.4s;
-
-  /* Shape + Size */
-  border-radius: 24px;
-  width: 100%;
-  height: 100%;
-  max-height: 45px;
-  max-width: 45px;
-
-  /* Display */
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  position: relative;
-  &:after {
-    position: absolute;
-    content: "[tone]";
-    color: white;
-    font-size: 14px;
-    top: 40px;
-  }
-  &:hover {
-    max-width: 60px;
-    max-height: 60px;
-  }
-
-  &:active {
-    background: white;
-  }
-
-  &:hover,
-  &:active {
-    &:after {
-      position: absolute;
-      content: "";
-      color: white;
-      font-size: 14px;
-      top: 40px;
-    }
-  }
-`;
-
-const SendButton = styled.button`
-  /* Color */
-  background: none;
-
-  /* Animation */
-  transition: all 0.4s;
-
-  /* Shape + Size */
-  border: none;
-  border-radius: 24px;
-  width: 100%;
-  height: 100%;
-  max-height: 50px;
-  max-width: 100;
-
-  /* Spacing */
-  margin: auto 5px;
-
-  /* Display */
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  outline: none;
-
-  &:active {
-    background: white;
-  }
-
-  &:hover {
-    background: white;
-  }
-
-  &:disabled {
-    &:hover {
-      background: none;
-    }
-    img {
-      opacity: 0.3;
-    }
-    cursor: not-allowed;
-  }
-`;
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -179,10 +32,6 @@ const useQuery = () => {
 
 // Main component
 const ChatRoom = () => {
-  interface RouteParams {
-    id: string;
-  }
-
   const urlquery = useQuery();
   const id = urlquery.get("room") as string;
 
@@ -196,8 +45,6 @@ const ChatRoom = () => {
 
   // State
   const [open, setOpen] = useState<boolean>(false);
-  const [scrolling, setScrolling] = useState<boolean>(false);
-  const [ID, setID] = useState<any>();
   const [tone, setTone] = useState<string>("");
   const [formValue, setFormValue] = useState<string>("");
 
@@ -222,10 +69,6 @@ const ChatRoom = () => {
     setFormValue("");
 
     dummy.current.scrollIntoView({ behavior: "smooth" }); // Scroll to latest message after send
-  };
-
-  var scrollToBottom = function () {
-    toneDrawer.current.scrollIntoView({ behavior: "smooth" });
   };
 
   // Logs + Side Effects
