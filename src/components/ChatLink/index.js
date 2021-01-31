da6eb36ce46264f1565c8fcf8f02c96b5ba2914c
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-01-30 22:39:59 -0600

    ChatLink now shows most recent message and timestamp

--- src/components/ChatLink/index.js
+++ src/components/ChatLink/index.js
@@ -5,10 +5,13 @@ import {
   ChatDeleteButton,
 } from "./styles/ChatLink";
 
-const ChatLink = ({ to, link }) => {
+const ChatLink = ({ to, recent_message, name }) => {
+  console.log("Message: ", recent_message);
   return (
     <ChatLinkWrapper>
-      <ChatLinkText to={to}>{link}</ChatLinkText>
+      <ChatLinkText to={to}>{name}</ChatLinkText>
+      {recent_message && <p>{recent_message.text}</p>}
+      {recent_message && <p>{recent_message.createdAt.toDate().toString()}</p>}
       <ChatDeleteButton>Delete</ChatDeleteButton>
     </ChatLinkWrapper>
   );
--- src/containers/ChatSelection/index.js
+++ src/containers/ChatSelection/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ChatLink from "../../components/ChatLink";
 import { useRecoilValue } from "recoil";
 import { chatRooms } from "../../atoms";
+import { firestore } from "../../firebase/config";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const ChatSelection = () => {
   const chats = useRecoilValue(chatRooms);
+  const [messageIndex, setMessageIndex] = useState({});
+
+  useEffect(() => {
+    chats.map((chat) => {
+      const messagesRef = firestore
+        .collection("rooms")
+        .doc(chat.id)
+        .collection("messages");
+
+      const message = messagesRef.orderBy("createdAt", "desc").limit(1);
+
+      message.get().then((msgs) => {
+        msgs.forEach((message) => {
+          setMessageIndex((prevmessageIndex) => {
+            var entry = { ...prevmessageIndex };
+            entry[chat.id] = message.data();
+            return entry;
+          });
+        });
+      });
+    });
+  }, [chats]);
 
   return (
     <div style={{ maxHeight: "400px", overflowY: "scroll" }}>
@@ -13,9 +37,10 @@ const ChatSelection = () => {
         chats.map((chat) => {
           return (
             <ChatLink
-              style={{ display: "block", color: "white" }}
+              style={{ display: "block" }}
               to={"/chat?room=" + chat.id}
-              link={chat.id}
+              name={chat.name}
+              recent_message={messageIndex[chat.id]}
             >
               {chat.id}
             </ChatLink>
