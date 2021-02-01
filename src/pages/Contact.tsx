a57870d4a3d01b3a5ab56a361e090132a4288ebf
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-02-01 16:01:53 -0600

    Began style setup for Contact Form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,81 +1,103 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "../components/Button";
+
+const ContactPageWrapper = styled.div``;
+const Section = styled.section``;
+const Container = styled.div``;
+const Grid = styled.div``;
+const Col = styled.div``;
+const Row = styled.div``;
+const ContactHeader = styled.h1``;
+const ContactForm = styled.form``;
+const Label = styled.label``;
+const Textarea = styled.textarea``;
 
 const Contact = () => {
   return (
-    <>
+    <ContactPageWrapper>
       <body className="body-2">
-        <div className="section-2">
-          <div className="w-container">
-            <div className="w-layout-grid grid">
-              <h1 id="w-node-Heading-25fe052f" className="heading-7">
-                Tell us what you think about speakEasy
-              </h1>
-              <h1 id="w-node-Heading 2-25fe052f" className="heading-8">
-                Contact Us
-              </h1>
-              <div id="w-node-af3780859513-25fe052f" className="w-htmlForm">
-                <form
-                  id="email-htmlForm"
-                  name="email-htmlForm"
-                  data-name="Email htmlForm"
+        <Section>
+          <Container>
+            <Grid className="w-layout-grid grid">
+              <Col>
+                <ContactHeader
+                  id="w-node-Heading-25fe052f"
+                  className="heading-7"
                 >
-                  <label htmlFor="name" className="field-label">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    className="text-field-2 w-input"
-                    maxLength={256}
-                    name="name"
-                    data-name="Name"
-                    placeholder=""
-                    id="name"
-                  />
-                  <label htmlFor="email-2" className="field-label-2">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    className="text-field w-input"
-                    maxLength={256}
-                    name="email"
-                    data-name="Email"
-                    placeholder=""
-                    id="email"
-                    required={false}
-                  />
-                  <label htmlFor="email" className="field-label-2">
-                    Message
-                  </label>
-                  <textarea
-                    placeholder="Example Text"
-                    maxLength={5000}
-                    id="field"
-                    name="field"
-                    required={false}
-                    className="textarea w-input"
-                  ></textarea>
-                  <input
-                    type="submit"
-                    value="Submit"
-                    data-wait="Please wait..."
-                    className="button w-button"
-                  />
-                </form>
-                <div className="w-htmlForm-done">
+                  Tell us what you think about speakEasy
+                </ContactHeader>
+              </Col>
+              <Col>
+                <h1 id="w-node-Heading 2-25fe052f" className="heading-8">
+                  Contact Us
+                </h1>
+                <Row id="w-node-af3780859513-25fe052f" className="w-htmlForm">
+                  <ContactForm
+                    id="email-htmlForm"
+                    name="email-htmlForm"
+                    data-name="Email htmlForm"
+                  >
+                    <Label htmlFor="name" className="field-Label">
+                      Name
+                    </Label>
+                    <input
+                      type="text"
+                      className="text-field-2 w-Input"
+                      maxLength={256}
+                      name="name"
+                      data-name="Name"
+                      placeholder=""
+                      id="name"
+                    />
+                    <Label htmlFor="email-2" className="field-Label-2">
+                      Email Address
+                    </Label>
+                    <input
+                      type="email"
+                      className="text-field w-Input"
+                      maxLength={256}
+                      name="email"
+                      data-name="Email"
+                      placeholder=""
+                      id="email"
+                      required={false}
+                    />
+                    <Label htmlFor="email" className="field-Label-2">
+                      Message
+                    </Label>
+                    <Textarea
+                      placeholder="Example Text"
+                      maxLength={5000}
+                      id="field"
+                      name="field"
+                      required={false}
+                      className="Textarea w-Input"
+                    ></Textarea>
+                    <Button
+                      type="submit"
+                      value="Submit"
+                      data-wait="Please wait..."
+                      className="button w-button"
+                    >
+                      Send
+                    </Button>
+                  </ContactForm>
+                  {/* <div className="w-htmlForm-done">
                   <div>Thank you! Your submission has been received!</div>
                 </div>
                 <div className="w-htmlForm-fail">
                   <div>
                     Oops! Something went wrong while submitting the htmlForm.
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                </div> */}
+                </Row>
+              </Col>
+            </Grid>
+          </Container>
+        </Section>
       </body>
-    </>
+    </ContactPageWrapper>
   );
 };
 
