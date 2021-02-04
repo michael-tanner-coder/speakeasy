d6061fec4ea7ccd6d2b53fec89e5046655113909
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-02-03 18:45:19 -0600

    Added default placeholders for profile icons and message times

--- src/components/ChatLink/index.js
+++ src/components/ChatLink/index.js
@@ -10,22 +10,25 @@ import {
   MessageDay,
 } from "./styles/ChatLink";
 import ProfileImage from "../ProfileImage";
+import DefaultIcon from "../../images/ProfileIcon.svg";
 
 const ChatLink = ({ to, recent_message, name }) => {
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   console.log("Message: ", recent_message);
   return (
     <ChatLinkWrapper to={to}>
-      {recent_message && (
-        <div>
-          <ProfileImage src={recent_message.photoURL} size="50px" />
-        </div>
-      )}
+      <div>
+        <ProfileImage
+          src={recent_message ? recent_message.photoURL : DefaultIcon}
+          size="50px"
+        />
+      </div>
+
       <ChatTextWrapper>
         <ChatName>{name}</ChatName>
         {recent_message && <RecentMessage>{recent_message.text}</RecentMessage>}
       </ChatTextWrapper>
-      {recent_message && (
+      {(recent_message && (
         <MessageDate>
           <MessageDay>
             {days[recent_message.createdAt.toDate().getDay()]}
@@ -33,6 +36,10 @@ const ChatLink = ({ to, recent_message, name }) => {
           <MessageHour>
             {recent_message.createdAt.toDate().toLocaleTimeString()}
           </MessageHour>
+        </MessageDate>
+      )) || (
+        <MessageDate>
+          <p>No messages</p>
         </MessageDate>
       )}
       {/* <ChatDeleteButton>Delete</ChatDeleteButton> */}
